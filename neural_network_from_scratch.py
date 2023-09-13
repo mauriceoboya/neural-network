367b91291e9547cef27e7742fd13df8df13ed3c3
mauriceoboya <mauriq97@gmail.com>
2023-09-13 11:05:34 +0300

    updated

--- neural_network_from_scratch.py
+++ neural_network_from_scratch.py
@@ -18,12 +18,22 @@ class Forwardpass:
         self.biases=nm.zeros((1,hidden_neurons))
     def outcome(self,inputs):
         self.output=nm.dot(inputs,self.weight)+self.biases
-        return f'{self.output}'
+        
+
+class Activation_Function:
+    def forward(self,input):
+        self.output=nm.maximum(0,input)
 
 layer1=Forwardpass(8,3)
+activation1=Activation_Function()
 layer2=Forwardpass(3,4)
+activation2=Activation_Function()
 layer3=Forwardpass(4,2)
 layer1.outcome(x1)
-layer2.outcome(layer1.output)
-layer3.outcome(layer2.output)
+activation1.forward(layer1.output)
+layer2.outcome(activation1.output)
+print(layer2.output)
+
+activation2.forward(layer2.output)
+layer3.outcome(activation2.output)
 print(layer3.output)
